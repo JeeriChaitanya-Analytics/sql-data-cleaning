c29acb2b025f6f04b02a6e79475fe2d0d7dc384e
✅ Jeeri chaitanya <chaituchaitanya347@gmail.com>
2026-02-17 12:06:25 +0530

    Update data_cleaning_queries.sql

--- sql/data_cleaning_queries.sql
+++ sql/data_cleaning_queries.sql
@@ -1,1 +1,21 @@
+-- Remove duplicates
+SELECT *
+FROM raw_data
+QUALIFY ROW_NUMBER() OVER (
+  PARTITION BY order_id
+  ORDER BY updated_at DESC
+) = 1;
+
+-- Standardize country names
+SELECT
+  CASE
+    WHEN country IN ('IND','IN') THEN 'India'
+    ELSE country
+  END AS country_cleaned
+FROM raw_data;
+
+-- Handle NULL values
+SELECT
+  COALESCE(customer_name, 'Unknown') AS customer_name
+FROM raw_data;
 
